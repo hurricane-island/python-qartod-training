816d575738f58581d3e5d7f7e77ef4ee8f655807
[Adele Jordan] <[ajae2021@mymail.pomona.edu]>
2025-07-02 12:31:05 -0400

    adding qartod test

--- qc_tests.py
+++ qc_tests.py
@@ -1,1 +1,17 @@
+from ioos_qc import qartod
 
+# QARTOD FLAGS:
+# 1 = pass
+# 2 = test not run on this data 
+# 3 = suspect
+# 4 = fail 
+# 9 = missing data
+
+
+results_gr = qartod.gross_range_test(
+    inp = [10.3, 68.1, 4.4, 71.9, 45.2, 12.5, 0.0, 100.0, 50.0, 5.0],
+    fail_span = [0, 100],
+    suspect_span = [15, 45]
+)
+
+print(results_gr)
