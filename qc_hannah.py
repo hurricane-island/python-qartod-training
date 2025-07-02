bd77ff99e011230d24980f281c2c1db140f95897
hmellor26 <mellorh22@gmail.com>
2025-07-02 15:34:38 -0400

    QC_hannah.py

--- qc_hannah.py
+++ qc_hannah.py
@@ -1,17 +1,14 @@
 
+from ioos_qc import qartod
 import pandas as pd
 from pandas import DataFrame, read_csv
 from datetime import datetime
 
 file_path = 'RG.csv'
-site_info: DataFrame = pd.read_csv(file_path, header=0) #reads all of the columns in file
-site_info.head()
+df: DataFrame = pd.read_csv(file_path, header=0) #reads all of the columns in file
+chlorophyll = df['Chlorophyll_RFU']
+# print (site_info.head())
 
-# #for our purposes, I only want to read select columns where the column headers are in the second row
-# df = pd.read_csv(file_path, header=1, usecols =  ["TIMESTAMP", "External_Temp", "Conductivity_us", "SpConductivity_us", "Salinity", "Chlorophyll_ugL", "Chlorophyll_RFU", "BGA_PE_RFU", "BGA_PE_ugL"])
-
-# df.drop(0, axis = 0, inplace=True) #I want to drop the first row below the headers which contain units
-# df.drop(1, axis = 0, inplace=True) #and the second row for analysis purposes 
 # df = df.reset_index(drop=True) #this resets the index 
 
 # timestamp = df["TIMESTAMP"].tolist()
@@ -29,10 +26,10 @@ site_info.head()
 # df.insert(loc=0, column = "TIMESTAMP_ISO", value = timestamp_clean) #this inserts the new time column to the leftmost 
 # print(df.head()) #This prints the first five rows of the dataframe
 
-# blank_count = df.isna().sum() #this will count the number of blanks in the df
+blank_count = df.isna().sum() #this will count the number of blanks in the df
 # print(f"Blank count: {blank_count}") 
 
-# duplicate_count = df.duplicated().sum() #this will count the number of duplicates in the df
+duplicate_count = df.duplicated().sum() #this will count the number of duplicates in the df
 # print(f"Duplicate count: {duplicate_count}")
 
 # Save the cleaned DataFrame to a new CSV file
@@ -46,6 +43,33 @@ site_info.head()
 # copy
 # merge
 
+# QARTOD FLAGS:
+# 1 = pass
+# 2 = test not run on this data 
+# 3 = suspect
+# 4 = fail 
+# 9 = missing data
 
+results_gr = qartod.gross_range_test(
+    inp = chlorophyll,
+    fail_span = [0, 100],
+    suspect_span = [0, 5]
+)
+suspect_fail_data = (results_gr>2).sum()
+print(suspect_fail_data)
+
+# print(df.describe())
+
+# Results Spike
+results_spike = qartod.spike_test(
+    inp = chlorophyll,
+    suspect_threshold = 0.25,
+    fail_threshold = 0.5,
+    method = "average"
+    
+)
+suspect_fail_spike = (results_spike>2).sum()
+print("suspect or fail", suspect_fail_spike)
+print("fail", (results_spike==4).sum())
 
 
