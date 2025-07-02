883022e8643c32ea48e49dc71ac796817db54a6f
[Adele Jordan] <[ajae2021@mymail.pomona.edu]>
2025-07-02 09:11:45 -0400

    adding qartod

--- qc_tests.py
+++ qc_tests.py
@@ -3,4 +3,13 @@ This will be playing around with the ioos_qc library
 Lesson: don't reinvent the wheel!!!
 '''
 
-from ioos_qc.config import Config
+from ioos_qc import qartod
+
+results = qartod.gross_range_test(
+    inp=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    suspect_span=[0, 8],
+    fail_span=[0, 10]
+)
+
+print(results)
+
